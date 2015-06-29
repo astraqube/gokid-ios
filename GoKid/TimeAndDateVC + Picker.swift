dc9e96435cbc53ad177aaa39ae73b39d09d8afd1
Bingwen Fu <bingwen@bingwen-mbp1.local>
2015-06-28 22:58:48 -0700

    asd

---
+++ GoKid/TimeAndDateVC + Picker.swift
@@ -0,0 +1,92 @@
+//
+//  TimeAndDateVC + Picker.swift
+//  GoKid
+//
+//  Created by Bingwen Fu on 6/28/15.
+//  Copyright (c) 2015 GoKid. All rights reserved.
+//
+
+
+extension TimeAndDateVC {
+    
+    // MARK: Date Time Picker setup
+    // --------------------------------------------------------------------------------------------
+    
+    func setupDateTimePicker() {
+        setupTimePicker()
+        setupDatePicker()
+    }
+    
+    func setupTimePicker() {
+        var h: CGFloat = 280
+        timePicker = DateTimePicker(frame: CGRectMake(0, view.h-h, view.w, h))
+        timePicker.backgroundColor = UIColor.whiteColor()
+        timePicker.addTargetForDoneButton(self, action: "timePickerDoneButtonClick")
+        timePicker.addTargetForCancelButton(self, action: "timePickerCancleButtonClick")
+    }
+    
+    func setupDatePicker() {
+        datePicker = THDatePickerViewController.datePicker()
+        datePicker.delegate = self
+        datePicker.setAllowClearDate(false)
+        datePicker.setClearAsToday(true)
+        datePicker.setAutoCloseOnSelectDate(false)
+        datePicker.setAllowSelectionOfSelectedDate(true)
+        datePicker.setDisableHistorySelection(true)
+        datePicker.setDisableFutureSelection(false)
+        datePicker.selectedBackgroundColor = UIColor.darkGrayColor()
+        datePicker.currentDateColor = UIColor.blackColor()
+        datePicker.currentDateColorSelected = UIColor.whiteColor()
+    }
+    
+    // MARK: Show Date Time Picker
+    // --------------------------------------------------------------------------------------------
+    
+    func showDatePicker() {
+        dateFormatter.dateFormat = "EE MMMM d, YYYY"
+        datePicker.date = NSDate()
+        var options = [
+            KNSemiModalOptionKeys.pushParentBack: false,
+            KNSemiModalOptionKeys.animationDuration: 0.3,
+            KNSemiModalOptionKeys.shadowOpacity: 0.3
+        ]
+        presentSemiViewController(datePicker, withOptions: options)
+    }
+    
+    func showTimePicker() {
+        dateFormatter.dateFormat = "hh:mm a"
+        timePicker.setMode(.Time)
+        if timePicker.superview == nil {
+            timePicker.alpha = 0.0
+            self.view.addSubview(timePicker)
+            timePicker.alphaAnimation(1.0, duration: 0.4, completion: nil)
+        }
+    }
+    
+    // MARK: Date Time Picker Delegate
+    // --------------------------------------------------------------------------------------------
+    
+    func datePickerDonePressed(datePicker: THDatePickerViewController!) {
+        var str = dateFormatter.stringFromDate(datePicker.date)
+        currentBindLabel?.text = str
+        currentBindModel?.valueString = str
+        updateCurrentUserCarpoolModel(currentBindModel!, date: timePicker.picker.date)
+        self.dismissSemiModalView()
+    }
+    
+    func datePickerCancelPressed(datePicker: THDatePickerViewController!) {
+        self.dismissSemiModalView()
+    }
+    
+    func timePickerDoneButtonClick() {
+        var str = dateFormatter.stringFromDate(timePicker.picker.date)
+        currentBindLabel?.text = str
+        currentBindModel?.valueString = str
+        updateCurrentUserCarpoolModel(currentBindModel!, date: timePicker.picker.date)
+        dismissDateTimePicker()
+    }
+    
+    func timePickerCancleButtonClick() {
+        dismissDateTimePicker()
+    }
+}
---
+++ GoKid/ViewControllers/VolunteerVC + ActionSheet.swift
@@ -0,0 +1,88 @@
+//
+//  VolunteerVC.swift
+//  GoKid
+//
+//  Created by Bingwen Fu on 6/28/15.
+//  Copyright (c) 2015 GoKid. All rights reserved.
+//
+
+extension VolunteerVC {
+    
+    // MARK: Handle Action Sheet
+    // --------------------------------------------------------------------------------------------
+    
+    func showActionSheet(cell: VolunteerCell) {
+        var row = self.tableView.indexPathForCell(cell)!.row
+        var model = self.tableData[row]
+        if model.taken {
+            showTakenActionSheet(cell, model: model)
+        } else{
+            showUntakenActionSheet(cell, model: model)
+        }
+    }
+    
+    func showTakenActionSheet(cell: VolunteerCell, model: VolunteerModel) {
+        let button1 = UIAlertAction(title: "Unvolunteer", style: .Default) { (alert) in
+            self.unRegisterVolunteerForCell(cell, model: model)
+        }
+        let button2 = UIAlertAction(title: "Cancel", style: .Cancel) { (alert) in
+            
+        }
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        alert.addAction(button1)
+        alert.addAction(button2)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func showUntakenActionSheet(cell: VolunteerCell, model: VolunteerModel) {
+        let button1 = UIAlertAction(title: "Volunteer", style: .Default) { (alert) in
+            self.registerVolunteerForCell(cell, model: model)
+        }
+        let button3 = UIAlertAction(title: "Volunteer All Drop-off", style: .Default) { (alert) in
+            self.registerVolunteerForCell(cell, model: model)
+        }
+        let button4 = UIAlertAction(title: "Volunteer Every Day", style: .Default) { (alert) in
+            self.registerVolunteerForCell(cell, model: model)
+        }
+        let button5 = UIAlertAction(title: "Assign team member", style: .Default) { (alert) in
+        }
+        let button6 = UIAlertAction(title: "Cancel", style: .Cancel) { (alert) in
+        }
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        alert.addAction(button1)
+        alert.addAction(button3)
+        alert.addAction(button4)
+        alert.addAction(button5)
+        alert.addAction(button6)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func unRegisterVolunteerForCell(cell: VolunteerCell, model: VolunteerModel) {
+        LoadingView.showWithMaskType(.Black)
+        self.dataManager.unregisterForOccurence(model.carpoolID, occurID: model.occrenceID) { (success, errStr) in
+            LoadingView.dismiss()
+            onMainThread() {
+                if success {
+                    cell.checkButton.backgroundColor = UIColor.lightGrayColor()
+                    model.taken = !model.taken
+                } else {
+                    self.showAlert("Fail to unvolunteer", messege: errStr, cancleTitle: "OK")
+                }
+            }
+        }
+    }
+    
+    func registerVolunteerForCell(cell: VolunteerCell, model: VolunteerModel) {
+        LoadingView.showWithMaskType(.Black)
+        self.dataManager.registerForOccurence(model.carpoolID, occurID: model.occrenceID) { (success, errStr) in
+            LoadingView.dismiss()
+            onMainThread() {
+                if success {
+                    cell.checkButton.backgroundColor = UIColor.greenColor()
+                    model.taken = !model.taken
+                } else {
+                    self.showAlert("Fail to volunteer", messege: errStr, cancleTitle: "OK")
+                }}
+        }
+    }
+}
