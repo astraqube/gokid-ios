8a92b43db7aaf7149bf7c6bb57fdd5f8bcde4209
bingwenfu <bingwen@bingwen-mbp1.local>
2015-07-05 03:20:34 -0700

    asd

---
+++ GoKid/ViewControllers/InviteRelationshipVC.swift
@@ -0,0 +1,35 @@
+//
+//  InviteRelationshipVC.swift
+//  GoKid
+//
+//  Created by Bingwen Fu on 7/5/15.
+//  Copyright (c) 2015 GoKid. All rights reserved.
+//
+
+import UIKit
+
+class InviteRelationshipVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func kidsFriendClick(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func mommyClick(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func daddyClick(sender: AnyObject) {
+    }
+    
+    @IBAction func sitterClick(sender: AnyObject) {
+    }
+    
+    @IBAction func otherClick(sender: AnyObject) {
+        
+    }
+    
+}
