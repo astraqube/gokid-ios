bd5b497d907b91bceebdde78a74b1024cf05fba6
Bingwen Fu <bingwen@fb.com>
2015-07-19 01:44:37 -0700

    update

--- GoKid/BaseFormVC.swift
+++ GoKid/BaseFormVC.swift
@@ -20,9 +20,29 @@ class BaseFormVC: XLFormViewController {
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     
+    var largeLeftButton: UIButton?
+    var largeRightButton: UIButton?
+    
     override func viewDidLoad() {
         self.initForm()
         super.viewDidLoad()
+        addLargeNavigationButton()
+    }
+    
+    func addLargeNavigationButton() {
+        var buttonW: CGFloat = 40
+        var buttonH: CGFloat = 36
+        var windowW = userManager.windowW
+        largeLeftButton = UIButton(frame: CGRectMake(0.0, 20.0, buttonW, buttonH))
+        largeRightButton = UIButton(frame: CGRectMake(windowW-buttonW, 20.0, buttonW, buttonH))
+        largeLeftButton?.addTarget(self, action: "leftNavButtonTapped", forControlEvents: .TouchUpInside)
+        largeRightButton?.addTarget(self, action: "rightNavButtonTapped", forControlEvents: .TouchUpInside)
+        if leftButton != nil {
+            view.insertSubview(largeLeftButton!, belowSubview: leftButton)
+        }
+        if rightButton != nil {
+            view.insertSubview(largeRightButton!, belowSubview: rightButton)
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
