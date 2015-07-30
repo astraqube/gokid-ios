0222f5e35bd5da4279b9d57d1591ca2a53b1c9c4
Dean Quiñanola <deanq@deanq.com>
2015-07-29 22:18:58 -0700

    model: InvitationModel

---
+++ GoKid/InvitationModel.swift
@@ -0,0 +1,25 @@
+//
+//  InvitationModel.swift
+//  GoKid
+//
+//  Created by Dean Quinanola on 7/29/15.
+//  Copyright (c) 2015 GoKid. All rights reserved.
+//
+
+import UIKit
+
+class InvitationModel: NSObject {
+    var inviteID: Int!
+    var inviter: RiderModel!
+    var carpool: CarpoolModel!
+
+    override init() {
+        super.init()
+    }
+
+    init(json: JSON) {
+        inviteID = json["id"].intValue
+        inviter = RiderModel(json: json["inviter"])
+        carpool = CarpoolModel(json: json["carpool"])
+    }
+}
