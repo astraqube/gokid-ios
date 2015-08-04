6556d3a9186c8f47767fc58e37261221e85481d9
Dean Quiñanola <deanq@deanq.com>
2015-08-04 16:11:47 -0700

    DEPRECATED: PhoneVerifyVC

--- GoKid/ViewControllers/PhoneVerifyVC.swift
+++ GoKid/ViewControllers/PhoneVerifyVC.swift
@@ -5,7 +5,7 @@
 //  Created by Bingwen Fu on 6/1/15.
 //  Copyright (c) 2015 GoKid. All rights reserved.
 //
-
+/* DEPRECATED
 import UIKit
 
 class PhoneVerifyVC: BaseFormVC {
@@ -91,3 +91,4 @@ class PhoneVerifyVC: BaseFormVC {
     }
 
 }
+*/
