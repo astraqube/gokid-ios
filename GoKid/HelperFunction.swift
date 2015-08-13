fa49d2b03816cf13555bf7617d26a4e1dff511b4
Dean Quiñanola <deanq@deanq.com>
2015-08-13 09:48:17 -0700

    helper: debounce()

--- GoKid/HelperFunction.swift
+++ GoKid/HelperFunction.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class HelperFunction: NSObject { }
 
+func debounce( delay:NSTimeInterval, #queue:dispatch_queue_t, action: (()->()) ) -> ()->() {
+
+    var lastFireTime:dispatch_time_t = 0
+    let dispatchDelay = Int64(delay * Double(NSEC_PER_SEC))
+
+    return {
+        lastFireTime = dispatch_time(DISPATCH_TIME_NOW,0)
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                dispatchDelay
+            ),
+            queue) {
+                let now = dispatch_time(DISPATCH_TIME_NOW,0)
+                let when = dispatch_time(lastFireTime, dispatchDelay)
+                if now >= when {
+                    action()
+                }
+        }
+    }
+}
+
 func rotationMake(degree: Double) -> CGAffineTransform {
     return CGAffineTransformMakeRotation(CGFloat(M_PI * (degree) / 180.0))
 }
