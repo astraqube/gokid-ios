12db64fb0e5d805efccc79001accf503bfc91dec
Dean Quiñanola <deanq@deanq.com>
2015-08-13 18:13:13 -0700

    fix: conditions for where to send user after VolunteerVC

--- GoKid/VolunteerVC.swift
+++ GoKid/VolunteerVC.swift
@@ -45,21 +45,21 @@ class VolunteerVC: BaseVC, UITableViewDelegate, UITableViewDataSource {
     // --------------------------------------------------------------------------------------------
     
     override func rightNavButtonTapped() {
-        if fromCarpoolList {
-            var vc = vcWithID("CarpoolEditVC") as! CarpoolEditVC
-            vc.occurrence = dataSource[1]
-            navigationController?.pushViewController(vc, animated: true)
-        } else {
-            if carpool.isOwner {
+        if carpool.isOwner {
+            if fromCarpoolList {
+                var vc = vcWithID("CarpoolEditVC") as! CarpoolEditVC
+                vc.occurrence = dataSource[1]
+                navigationController?.pushViewController(vc, animated: true)
+            } else {
                 var vc = vcWithID("InviteParentsVC") as! InviteParentsVC
                 vc.carpool = self.carpool
                 vc.hideForwardNavigationButtons = false
                 self.navigationController?.pushViewController(vc, animated: true)
-            } else {
-                var vc = vcWithID("CarpoolSucceedVC") as! CarpoolSucceedVC
-                vc.carpool = self.carpool
-                navigationController?.pushViewController(vc, animated: true)
             }
+        } else {
+            let appDelegate  = UIApplication.sharedApplication().delegate as! AppDelegate
+            let mainController = appDelegate.window!.rootViewController as! MainStackVC
+            mainController.determineStateForViews()
         }
     }
     
